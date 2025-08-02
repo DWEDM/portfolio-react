bdf5074640efa765be01af50e9b1b7386e474f24
wane <denver.delamasa@gmail.com>
2025-08-03 01:59:47 +0800

    What I'm currently Working on section on the homepage

---
+++ src/components/cards/CurrentlyWorkingCards.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import freecodecamp from '../../assets/gallery/freecodecamp-first-activity.png';
+import kodekloud from '../../assets/gallery/KodeKloud-Docker.png';
+
+const ProjectCards = () => {
+    return(
+        <>
+            <div className="fade-up-scroll py-4 flex lg:flex-row flex-col-reverse gap-4">
+                <figure className="md:block md:w-1/2 lg:w-2/5 flex items-center">
+                    <img 
+                        src={freecodecamp} 
+                        alt="DENR PENRO" 
+                        className="rounded-xl w-full h-full object-cover"
+                    />
+                </figure>
+                <div className="flex flex-col gap-2 my-auto md:w-1/2 lg:w-3/5">
+                    <h2 className="card-title text-2xl">freecodecamp.org | Responsive Web Design</h2>
+                    <p className="text-sm mb-2 p-0">Started at July 2025</p>  
+                    <p className="text-sm p-0">
+                        Currently working on how to further improve my CSS knowledge by revisiting the fundamentals 
+                        of creating classic HTML5 website     
+                    </p>
+                </div>
+            </div>
+            <div className="divider my-4"></div>
+                <div className="fade-up-scroll py-4 flex lg:flex-row flex-col-reverse gap-4">
+                    <figure className="md:w-1/2 lg:w-2/5 flex items-center">
+                        <img 
+                            src={kodekloud} 
+                            alt="DENR PENRO" 
+                            className="rounded-xl my-auto w-full h-full object-cover"
+                        />
+                    </figure>
+                <div className="flex flex-col gap-2 my-auto md:w-1/2 lg:w-3/5">
+                    <h2 className="card-title text-2xl">KodeKloud | Docker Training Course for the Absolute Beginner</h2>
+                    <p className="text-sm mb-2 p-0">Started at July 2025</p>  
+                    <p className="text-sm p-0">
+                        Trying to learn more about DevOps and to understand it's principles out of curiosity starting with Docker. 
+                        But I realized how it is important in modern software development and operations teams
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProjectCards;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import ProjectCards from '../cards/ProjectCards';
+import WorkedWithCards from '../cards/WorkedWithCards';
+import CurrentlyWorkingCards from '../cards/CurrentlyWorkingCards';
 import HomeAnimation from '../cards/HomeAnimation';
 import GitHubStats from '../GitHubStats';
 
@@ -8,7 +9,7 @@ const Home = ({ themeName }) => {
   return (
     <>
         <section id="Home" className="hero mx-auto bg-noise min-h-screen flex flex-col gap-y-12">
-            <div className="hero-content flex-col-reverse lg:gap-x-4 lg:flex-row-reverse">
+            <div className="hero-content flex-col-reverse lg:gap-x-4 lg:flex-row">
                 <div className="w-auto">
                     <h1 className="text-6xl font-bold fade-up-scroll">Technical, Creative, & Methodical.</h1>
                     <p className="py-6 fade-up-scroll">
@@ -46,13 +47,25 @@ const Home = ({ themeName }) => {
                 <GitHubStats themeName={themeName} />   
             </div>
             <div id="" className="hero-content h-auto w-full justify-start">
-                <div className="flex-col lg:flex-row-reverse fade-up-scroll">
-                    <h1 className="text-4xl font-bold mb-2">What I’ve Worked On:</h1>
-                    <p className="text-base-content mb-7 text-md">
-                        A showcase of the projects and experiences that shaped me...
-                    </p>
-                    <div className="w-full h-auto flex-wrap flex gap-8">
-                        <ProjectCards/>
+                <div className="flex flex-col gap-y-12">
+                    <div className='fade-up-scroll'>
+                        <h1 className="text-4xl font-bold mb-2">What I’ve Worked On:</h1>
+                        <p className="text-base-content mb-7 text-md">
+                            A showcase of the projects and experiences that shaped me...
+                        </p>
+                        <div className="w-full h-auto flex-wrap flex gap-8">
+                            <WorkedWithCards/>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className='fade-up-scroll'>
+                        <h1 className="text-4xl font-bold mb-2">What I'm currently up to:</h1>
+                        <p className="text-base-content mb-7 text-md">
+                            Things I am currently working on to improve my current knowledge...
+                        </p>
+                        <div className="w-full h-auto flex-col flex gap-2 px-12">
+                            <CurrentlyWorkingCards/>
+                        </div>
                     </div>
                 </div>
             </div>
